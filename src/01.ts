2f5c011d6f56adeef313e5e0deeecdb9e41edd99
Arnis Jaundžeikars <sangsatori@theradiant.space>
2024-12-01 18:10:44 +0000

    cleanup code

--- src/01.ts
+++ src/01.ts
@@ -1,55 +1,51 @@
 import { Solver, reNumbers } from "./utils.ts";
 
-// right, had a generic to infer turn type...
+/**
+ * Gets next smallest pair from both inputs
+ */
+const getNextSmallestPair = function* (L: number[], R: number[]) {
+    const length = L.length;
+    let i = 0;
+    while (i < length) {
+        yield [L[i], R[i]].sort();
+        i++;
+    }
+}
+
 const fn: Solver<number> = async (lines) => {
-    // uh how I do these definitions right...
     const L: number[] = [];
     const R: number[] = [];
-
-    let distanceSum = 0;
-    let similarityScore = 0;
-
-    // we need to load both in memory
-    let i = 0;
-    for await (const line of lines) {
-        for (const [token] of line.matchAll(reNumbers)) {
-            (i % 2 ? R : L).push(Number.parseInt(token, 10));
-            i++;
-        }
+    {
+        let i = 0;
+        for await (const line of lines)
+            for (const [token] of line.matchAll(reNumbers)) {
+                (i % 2 ? R : L).push(Number.parseInt(token, 10));
+                i++;
+            }
     }
-    // NOW WE HAVE COMPLETE LISTS
-
-    const length = L.length;
-
-    // sorts in-place
-    L.sort();
-    R.sort();
+    L.sort(); R.sort();
 
     const countsInR = R.reduce(
-        (counts, k) => counts.set(k, (counts.get(k) || 0) + 1),
+        (counts, k) =>
+            counts.set(
+                k,
+                (counts.get(k) || 0) + 1),
         new Map<number, number>());
+
+    {
+        let distanceSum = 0;
+        let similarityScore = 0;
+        for (const [a, b] of getNextSmallestPair(L, R))
+            distanceSum += (b - a);
     
-    // TODO: docs on always returning in ascending order
-    const getSmallestPair = function* () {
-        let i = 0;
-        while (i < length) {
-            // maybe ensure they're always in ascending order?
-            yield [L[i], R[i]].sort();
-            i++;
-        }
+        for (const k of L)
+            similarityScore += k * (countsInR.get(k) || 0 );
+    
+        return [
+            distanceSum,
+            similarityScore
+        ];
     }
-
-    // NOW ACTUALLY COMPUTE RESULTS
-    for (const [a, b] of getSmallestPair())
-        distanceSum += (b - a);
-
-    for (const k of L)
-        similarityScore += k * (countsInR.get(k) || 0 );
-
-    return [
-        distanceSum,
-        similarityScore
-    ];
 }
 
 export default fn;
