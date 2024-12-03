c4fc2e4c4640baad2166b33279deb5b318fa7fc5
Arnis Jaundžeikars <sangsatori@theradiant.space>
2024-12-03 06:42:49 +0000

    feat: day 3 part 2

--- src/03.ts
+++ src/03.ts
@@ -1,19 +1,28 @@
 import { Solver } from "./utils.ts";
 
-const re = /mul\((\d+),(\d+)\)/g;
-
+// const re = /mul\((\d+),(\d+)\)/g;
+const re = /(?<enable>do\(\))|(?<disable>don't\(\))|mul\((?<x>\d+),(?<y>\d+)\)/g;
 
 const fn: Solver<number> = async (lines) => {
-    let sum = 0;
+    let sum1 = 0;
+    let sum2 = 0;
+    let enabled = true;
     for await (const line of lines) {
-        for (let [, a, b] of line.matchAll(re)) {
-            sum += Number.parseInt(a) * Number.parseInt(b);
+        for (const [, enable, disable, x, y] of line.matchAll(re)) {
+            if (enable)
+                enabled = true;
+            if (disable)
+                enabled = false
+            if (x && y) {
+                const product = Number.parseInt(x) * Number.parseInt(y);
+                sum1 += product;
+                if (enabled)
+                    sum2 += product;
+            }
         }
     }
-    return [
-        sum,
-        0
-    ];
+
+    return [sum1, sum2];
 }
 
 export default fn;
