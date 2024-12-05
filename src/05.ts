4590e07ae0d59d35333abc5ecfea38bd72eb4fd1
Arnis Jaundžeikars <sangsatori@theradiant.space>
2024-12-05 13:33:13 +0000

    refactor(day5): avoid needlessly creating an intermediate lookup structure

--- src/05.ts
+++ src/05.ts
@@ -9,38 +9,35 @@ const fn: Solver<number> = async (lines) => {
         const rules: Map<string, Set<string>> = new Map();
         let isParsingRules = true;
         for await (const line of lines) {
-            if (!line.length) {
+            // no need to parse as numbers yet - it's all just tokenization
+
+            if (isParsingRules) // pt. 1: parsing & building rules
+            if (line.length) for (const { groups: { before, after } = {}} of line.matchAll(re)) {
+                if (!rules.has(after))
+                    rules.set(after, new Set());
+                rules.get(after)?.add(before);
+            } else {
                 isParsingRules = false;
                 continue;
-            }
-    
-            if (isParsingRules) { // first part, building rules
-                for (const { groups: { before, after } = {}} of line.matchAll(re)) {
-                    if (!rules.has(after))
-                        rules.set(after, new Set());
-                    rules.get(after)?.add(before);
-                }
-            } else { // second part, parsing and validating
-                // we don't even need .map(s => Number.parseInt(s))
-                // it's just tokenization at first
+            } else { // pt. 2: validating page sequences
                 const tokens = line.split(',');
-                const positions = tokens.reduce((lookup, token, i) => lookup.set(token, i), new Map());
                 let isValid = true;
-    
+
                 positionValidation:
-                for (const [token, position] of positions) if (rules.has(token))
-                    for (const mustPrecede of rules.get(token) ?? [])
-                        if (positions.has(mustPrecede))
-                        if (positions.get(mustPrecede) > position) {
+                for (let position = 0; position < tokens.length; position++) {
+                    const token = tokens[position];
+
+                    if (rules.has(token)) for (const mustPrecede of rules.get(token) ?? []) {
+                        const mustPrecedeIndex = tokens.indexOf(mustPrecede);
+                        if (mustPrecedeIndex > -1 && mustPrecedeIndex > position) {
                             isValid = false; // invalidated
                             break positionValidation;
                         } // else we're good, continue checkining
+                    }
+                }
 
-                if (!isValid) 
-                    tokens.sort((after, before) => 
-                        rules.get(before)?.has(after)
-                            ? -1
-                            : 1);
+                if (!isValid) tokens.sort((after, before) => 
+                    rules.get(before)?.has(after) ? -1 : 1);
 
                 result[isValid ? 0 : 1] += Number.parseInt(tokens[(tokens.length-1) / 2]);
             }
